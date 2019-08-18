b838b31354fb905cf6fad4f7f8f953b0b46975d7
Astrid Yu <astrid@astrid.tech>
2019-08-18 05:49:25 +0100

    Status information

--- octoprint_quadfrost/__init__.py
+++ octoprint_quadfrost/__init__.py
@@ -2,29 +2,57 @@
 from __future__ import absolute_import
 
 
-import octoprint.plugin
+import octoprint.plugin as opl
 import flask
 
 from . import quadfrost
 
+EVENT_STATUS_MAP = {
+    'Connected': 1,
+    'Disconnected': 7,
+    'PrintDone': 5,
+    'PrintResumed': 2,
+    'PrintStarted': 2,
+    'PrintPaused': 6
+}
+
 class QuadfrostPlugin(
-    octoprint.plugin.BlueprintPlugin,
-    octoprint.plugin.StartupPlugin,
-    octoprint.plugin.ShutdownPlugin,
-    octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin):
+    opl.BlueprintPlugin,
+    opl.StartupPlugin,
+    opl.ShutdownPlugin,
+    opl.ProgressPlugin,
+    opl.SettingsPlugin,
+    opl.AssetPlugin,
+    opl.EventHandlerPlugin,
+    opl.TemplatePlugin):
+    
+    quadfrost = None
 
     def on_after_startup(self):
         port = self._settings.get(['port'])
         self._logger.info('Connecting to Arduino at %s', port)
-        self.quadfrost = None
         try:
             self.quadfrost = quadfrost.QuadFrost(port)
             self._logger.info("Successfully connected to Arduino at %s", port)
         except Exception as e:
             self._logger.error("Error while initializing Arduino", exc_info=True)
     
+    def on_print_progress(self, storage, path, progress):
+        if self.quadfrost is not None:
+            self.quadfrost.set_status(2)
+            self.quadfrost.set_progress(progress)
+    
+    def on_event(self, event, payload):
+        if self.quadfrost is not None:
+            if event in EVENT_STATUS_MAP:
+                code = EVENT_STATUS_MAP[event]
+                self._logger.info("Received %s event. Setting status to %s.", event, code)
+                self.quadfrost.set_status(code)
+            elif event == 'PrintFailed':
+                reason = payload['reason']
+                code = {'cancelled': 3, 'error': 4}[reason]
+                self._logger.info("Received %s event with reason %s. Setting status to %s.", event, reason, code)
+
     def get_template_configs(self):
         return [
             dict(type="tab", custom_bindings=False)
@@ -32,7 +60,7 @@ class QuadfrostPlugin(
 
     def get_settings_defaults(self):
         return dict(
-            port='/dev/ttyACM0'
+            port='/dev/ttyUSB0'
         )
 
     def get_assets(self):
@@ -42,7 +70,7 @@ class QuadfrostPlugin(
             less=["less/quadfrost.less"]
         )
 
-    @octoprint.plugin.BlueprintPlugin.route("/lamp", methods=["POST"])
+    @opl.BlueprintPlugin.route("/lamp", methods=["POST"])
     def endpoint_lamp(self):
         r = int(flask.request.values['red'])
         g = int(flask.request.values['green'])
@@ -55,7 +83,7 @@ class QuadfrostPlugin(
             self._logger.warn("Failed to set Arduino: not connected")
             return "", 503
 
-    @octoprint.plugin.BlueprintPlugin.route("/filter", methods=["POST"])
+    @opl.BlueprintPlugin.route("/filter", methods=["POST"])
     def endpoint_filter(self):
         power = int(flask.request.values['power'])
         if self.quadfrost is not None:
@@ -64,7 +92,7 @@ class QuadfrostPlugin(
         else:
             self._logger.warn("Failed to set Arduino: not connected")
         return ""
-
+    
     def on_shutdown(self):
         if self.quadfrost is not None:
             self._logger.info("Closing serial")
